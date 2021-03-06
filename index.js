9926c4ae44f96d1fbb482c6d8d8667098fda4c1a
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-03-06 16:07:45 +0500

    Added bloglist backend step2

--- index.js
+++ index.js
@@ -1,61 +1,8 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-    transform: (document, requiredDocument) => {
-        requiredDocument.id = requiredDocument._id.toString()
-        delete requiredDocument._id
-        delete requiredDocument.__v
-    }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-const password = process.env.password
-const mongoUrl = `mongodb+srv://fullstackopen:${password}@cluster0.2dcnc.mongodb.net/blogList?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        console.log('Success! Connected to DB')
-    })
-    .catch(error => {
-        console.error('DB Connection Error:',error.message)
-    })
-
-app.use(cors())
-app.use(express.json())
-morgan.token('body', (req,res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, requiredDocument) => {
+        requiredDocument.id = requiredDocument._id.toString()
+        delete requiredDocument._id
+        delete requiredDocument.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
