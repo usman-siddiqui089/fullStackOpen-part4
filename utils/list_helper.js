963322de1842b6ee74400d4aef5520fb969b65e3
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-03-06 19:14:21 +0500

    Added bloglist unit tests step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const sumOfPosts = (blogPosts) => {
+    const sum = blogPosts.reduce((sum, blog) => {
+        return sum + blog.likes
+    }, 0)
+
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    sumOfPosts
 }
