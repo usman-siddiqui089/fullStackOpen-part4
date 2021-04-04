a735f5768d6143081d83951dcb1221dc56912f0d
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-04-04 23:09:38 +0500

    added blogList api tests step3

--- tests/blogList_api.test.js
+++ tests/blogList_api.test.js
@@ -12,11 +12,32 @@ test('returns correct amount of blogs', async () => {
     expect(response.body).toHaveLength(2)
 })
 
-test.only('blog contains valid id', async () => {
+test('blog contains valid id', async () => {
     const response = await api.get('/api/blogs').expect(200)
     const result = response.body.map(blog => {
         expect(blog.id).toBeDefined()
     })
+})
+
+test.only('a blog is added successfully', async () => {
+    const blogsBeforePost = await api.get('/api/blogs')
+    const newBlog = {
+        title : "Another good blog",
+        author : "David",
+        url : "https://blog.com",
+        likes : 90
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await api.get('/api/blogs')
+    expect(blogsAtEnd.body).toHaveLength(blogsBeforePost.body.length + 1)
+
+    const titles = blogsAtEnd.body.map(blog => blog.title)
+    expect(titles).toContain('Another good blog')
 })
 
 afterAll(() => {
